1e5b74210d46a538be625683fd78fa3695113793
Skuffy <skuffy808@gmail.com>
2025-04-10 23:26:22 +1200

    Add better movement

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 200.0
+const JUMP_VELOCITY = -200.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,12 +12,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Always move right
+	velocity.x = SPEED
 
 	move_and_slide()
